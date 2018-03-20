85b479d7feab8f6bf8614795d8d99b11625d2860
ryantbrennan1 <ryantbrennan1@gmail.com>
2018-03-20 15:08:42 -0400

    Adding rollbar

--- dbt/ui/colors.py
+++ dbt/ui/colors.py
@@ -5,5 +5,6 @@ COLORS = {
     'red': colorama.Fore.RED,
     'green': colorama.Fore.GREEN,
     'yellow': colorama.Fore.YELLOW,
+    'cyan': colorama.Fore.CYAN,
     'reset_all': colorama.Style.RESET_ALL
 }
